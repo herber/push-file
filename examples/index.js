18e406bef23f180a0286a28678680c0004c7010d
Tobi <tobihrbr@gmail.com>
2016-10-23 18:19:23 +0200

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@ const pushFile = require ('push-file');
 
 console.log(__dirname + 'text.txt');
 
-pushFile('http://localhost/profile', __dirname + '/test.txt', function (err, body) {
+pushFile('http://localhost/upload', __dirname + '/test.txt', function (err, body) {
     if (err) {
         console.log(err);
     } else {
